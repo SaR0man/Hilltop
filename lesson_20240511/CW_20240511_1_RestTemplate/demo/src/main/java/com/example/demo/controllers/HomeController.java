f0f786872efa5c92045a57684a48e8a3e6c13aa7
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-05-23 23:16:14 +0300

    update

--- lesson_20240511/CW_20240511_1_RestTemplate/demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ lesson_20240511/CW_20240511_1_RestTemplate/demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,34 +1,33 @@
 package com.example.demo.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/home")
 public class HomeController {
 
-    private final String apiKey = "2c9d65d5";
+    private final String apiKey = "5c3f2d56";
     private final String baseUrl = "https://www.omdbapi.com/";
 
     @GetMapping("/search")
     public void test(String title) {
         RestTemplate restTemplate = new RestTemplate();
         String url = baseUrl + "?apikey=" + apiKey + "&s=" + title;
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
 
-        System.out.println(HttpStatus.NOT_FOUND.getReasonPhrase());
-        System.out.println(HttpStatus.NOT_FOUND.value());
+        if (response.getBody().containsKey("Search")) {
+            ArrayList<Map<String, String>> search = (ArrayList<Map<String, String>>) response.getBody().get("Search");
 
-        if (response.getStatusCode().is2xxSuccessful()) {
-            if (response.hasBody())
-                System.out.println("Response status code : " + response.getBody());
-            else {
-                System.out.println("Response not exist body");
-            }
+            search.forEach(x -> {
+                System.out.println(x.get("Title"));
+            });
         }
     }
 }
