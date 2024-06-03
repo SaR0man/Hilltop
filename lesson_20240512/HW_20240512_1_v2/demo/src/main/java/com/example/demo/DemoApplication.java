3c16836b8062eab1194493644211f5747f1a28cd
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-06-03 22:39:53 +0300

    update

---
+++ lesson_20240512/HW_20240512_1_v2/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		requestFactory.setOutputStreaming(false);
+		int second = 3;
+		requestFactory.setConnectTimeout(second * 1000);
+		requestFactory.setReadTimeout(second * 1000);
+		restTemplate.setRequestFactory(requestFactory);
+		return restTemplate;
+	}
+}
