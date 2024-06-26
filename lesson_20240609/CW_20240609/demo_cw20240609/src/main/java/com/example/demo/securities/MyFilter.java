c6628708745fafa438a9b1d1256933f98b74f7d6
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2024-06-26 13:08:47 +0300

    update

---
+++ lesson_20240609/CW_20240609/demo_cw20240609/src/main/java/com/example/demo/securities/MyFilter.java
@@ -0,0 +1,33 @@
+package com.example.demo.securities;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+@Component
+public class MyFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        System.out.print("Request " + request.getRemoteAddr() + " time : " + LocalTime.now());
+        if (request.getHeader("apiKey") == null || !request.getHeader("apiKey").equals("secret12345")) {
+            servletResponse.getWriter().write("Api key not correct");
+            System.out.println(" access denied (отказано в доступе)");
+
+        }else{
+            System.out.println(" access received (доступ получен)");
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+    }
+
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+}
